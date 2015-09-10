abe651af2dedfee00fb95f49911129ae14c952fd
kennethnorman <norman.kenneth@gmail.com>
2015-09-10 19:13:07 +0100

    fix

--- list.bat
+++ list.bat
@@ -1,6 +1,8 @@
 @echo off
 echo %DATE% >list.log
 
+set tempfolder=c:\temp\_Upload
+
 XMLDirectory songlist songlist.xml -obs >> list.log
 XMLDirectory songinfo songinfo.xml -obs >> list.log
 
@@ -8,10 +10,12 @@ type songupd_top.xml > songupd.xml
 echo %DATE% >> songupd.xml
 type songupd_bot.xml >> songupd.xml
 
-del /q ..\_Upload\*
-copy scores.ftp ..\_Upload
-for /D %%i IN (*) do copy "%%i\*.pdf" ..\_Upload /y
-del /q ..\_Upload\_private*
+if not exist %tempfolder% md %tempfolder%
+del /q %tempfolder%\*
+copy scores.ftp %tempfolder%
+copy songupd.xml %tempfolder%
+for /D %%i IN (*) do copy "%%i\*.pdf" %tempfolder% /y
+del /q %tempfolder%\_private*
 
 if exist list.temp del list.temp
 if exist list.txt rename list.txt list.temp
@@ -19,7 +23,7 @@ dir /s *.pdf /w /b /on >>list.txt
 
 ftp -s:list.ftp
 
-pushd ..\_Upload
+pushd %tempfolder%
 ftp -s:scores.ftp
 7z a scores.zip *.pdf
 popd
